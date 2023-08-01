bd4c7882448d77575abd44a1b7e838b022997e68
Henrique <henriquesg09@hotmail.com>
2023-08-01 08:28:49 -0300

    feat: remove update on days 0, 1 and 7

--- src/server/api/routers/daily.ts
+++ src/server/api/routers/daily.ts
@@ -3,20 +3,22 @@ import {
   publicProcedure,
 } from '~/server/api/trpc';
 
-function pause(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export const dailyRouter = createTRPCRouter({
   getCurrent: publicProcedure.query(async ({ ctx }) => {
     const daily = await ctx.prisma.daily.findFirst();
     const employees = await ctx.prisma.employee.count();
 
     const currentDate = new Date().toISOString().split('T')[0];
+    const currentDay = new Date().getDay();
 
     let currentPosition;
 
-    if (daily?.current_day === currentDate) {
+    if (
+      daily?.current_day === currentDate ||
+      currentDay === 0 ||
+      currentDay === 1 ||
+      currentDay === 7
+    ) {
       currentPosition = daily?.current_position ?? 0;
     } else {
       currentPosition =
@@ -38,8 +40,6 @@ export const dailyRouter = createTRPCRouter({
         position: currentPosition,
       },
     });
-
-    await pause(3000);
 
     return {
       employee: { ...currentEmployee },
